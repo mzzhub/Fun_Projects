a07f738b47146fad329e5e0836a4f2a4a9943005
mzhub <mishabhadhi@gmail.com>
2024-08-17 23:34:38 +0530

    Add files via upload

--- multiplication (2,3,5).py
+++ multiplication (2,3,5).py
@@ -18,25 +18,32 @@ for i in permutation:
     # Making five-digit number from 5th to 9th index.
     c = i[5]*10000 + i[6]*1000 + i[7]*100 + i[8]*10 + i[9]
 
-    # Removing occurences of, zeroes comming as the left most digit in a,b,c
-    if i[0] == 0 or i[2] == 0 or i[5] == 0:
-        continue
 
     # Checking condition and printing the result.
     if a*b==c:
-        print(a,"X",b,"=",c)
-
+        if c < 10000:
+            # If the product is a four-digit, this will add a leading zero to the output.
+            print(a,"X",b,"=","0"+str(c))
+        else:
+            print(a,"X",b,"=",c)
 
 
 
 
 
 #   Output
+#       12 X 483 = 05796
+#       18 X 297 = 05346
+#       27 X 198 = 05346
 #       27 X 594 = 16038
+#       28 X 157 = 04396
 #       36 X 495 = 17820
+#       39 X 186 = 07254
 #       39 X 402 = 15678
+#       42 X 138 = 05796
 #       45 X 396 = 17820
 #       46 X 715 = 32890
+#       48 X 159 = 07632
 #       52 X 367 = 19084
 #       54 X 297 = 16038
 #       63 X 927 = 58401
